73b2c57b1a4a22291ab7e74e61806976c8dc0714
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-01-25 00:20:54 +0300

    polishing up of the code and also addition of an email listing text file where emails can be added and picked by program

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import random
 
 final_quote_list = []
 now = dt.datetime.now()
-if now.weekday() == 1:
+working_days = [0, 1, 2, 3, 4]
+if now.weekday() in working_days:
     with open("quotes.txt") as quotes:
         quotes_list = quotes.readlines()
         for quote in quotes_list:
@@ -13,12 +14,13 @@ if now.weekday() == 1:
 
     random_quote = random.choice(final_quote_list)
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        connection.starttls()
-        connection.login(user="leumaselulakk@gmail.com", password="the_password:)")
-        connection.sendmail(
-            from_addr="leumaselulakk@gmail.com",
-            to_addrs="samuelkibirigek@gmail.com",
-            msg=f"Subject:Quote of the day\n\n{random_quote}"
-        )
-
+    with open("email_listing.txt") as emails:
+        for email in emails.readlines():
+            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+                connection.starttls()
+                connection.login(user="kibirigekalules@gmail.com", password="my_password")
+                connection.sendmail(
+                    from_addr="kibirigekalules@gmail.com",
+                    to_addrs=f"{email}",
+                    msg=f"Subject:Quote of the day\n\n{random_quote}"
+                )
